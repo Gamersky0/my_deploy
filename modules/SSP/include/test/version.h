27906c52a849d238421805ee3b7df6edaf418f11
root <root@chimytest.momenta.ai>
2023-04-20 17:49:00 +0800

    my_deploy

---
+++ modules/SSP/include/test/version.h
@@ -0,0 +1,21 @@
+#ifndef TEST_VERSION_H
+#define TEST_VERSION_H
+
+#define TEST_MAJOR_VERSION 0
+#define TEST_MINOR_VERSION 0
+#define TEST_PATCH_VERSION 1
+#define TEST_VERSION "0.0.1"
+#define TEST_VERSION_FULL "0.0.1"
+
+#define GIT_BRANCH "sspv2/dev"
+#define GIT_COMMIT_HASH "45b27d14"
+#define GIT_COMMIT_COUNT 298
+#define GIT_COMMIT_DATE "2023/04/14 14:42:46"
+#define GIT_DIFF_NAME_ONLY "conanfile.py,ssp_v2/Generator/vsomeip_generator/CMakeLists.txt,"
+
+#define USERNAME_HOSTNAME "root@mm-Nuvo-8108GC-Series"
+#define PROJECT_SOURCE_DIR "/home/mm/workspace/SSP_service"
+#define PROJECT_BINARY_DIR "/home/mm/workspace/SSP_service/build"
+#define DATA_DIR "/home/mm/workspace/SSP_service/data"
+
+#endif  // TEST_VERSION_H
